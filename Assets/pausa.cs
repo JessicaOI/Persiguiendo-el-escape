7f56dcc28154dcffc8d23f1c9d68cf7edb76dbd0
Jessica Ortiz <jessicaortiz02b@gmail.com>
2024-03-05 19:57:39 -0600

    trabajando pausa

---
+++ Assets/pausa.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausa : MonoBehaviour
+{
+
+    public void setPausa() {
+        Time.timeScale = 0f;
+    }
+
+}
