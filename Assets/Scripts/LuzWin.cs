9fd568e55e94fb684a92f5b0545085438563c9a9
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2024-03-08 09:26:26 -0600

    Logica de Ganar

--- Assets/Scripts/LuzWin.cs
+++ Assets/Scripts/LuzWin.cs
@@ -1,51 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LuzWin : MonoBehaviour
 {
-    public GameObject[] objetosActivables; // Lista de objetos que se pueden activar o desactivar
-    public int objetosActivadosNecesarios = 3; // Número de objetos que deben estar activados
-    public int objetosDesactivadosNecesarios = 2; // Número de objetos que deben estar desactivados
+    public GameObject objetoControlado;
+    private bool encendido = true;
 
-    private void Start()
+    public void CambiarEstado()
     {
-        // Desactivar todos los objetos al principio
-        foreach (GameObject objeto in objetosActivables)
+        encendido = !encendido;
+        objetoControlado.SetActive(encendido);
+        
+        if (encendido)
         {
-            objeto.SetActive(false);
+            Debug.Log(objetoControlado.name + " está encendido.");
         }
-    }
-
-    public void ActivarDesactivarObjeto(int index)
-    {
-        GameObject objeto = objetosActivables[index];
-        objeto.SetActive(!objeto.activeSelf); // Cambiar estado del objeto (activado/desactivado)
-        VerificarCondiciones();
-    }
-
-    private void VerificarCondiciones()
-    {
-        int objetosActivados = 0;
-        int objetosDesactivados = 0;
-
-        // Contar objetos activados y desactivados
-        foreach (GameObject objeto in objetosActivables)
+        else
         {
-            if (objeto.activeSelf)
-            {
-                objetosActivados++;
-            }
-            else
-            {
-                objetosDesactivados++;
-            }
-        }
-
-        // Verificar si se cumplen las condiciones
-        if (objetosActivados == objetosActivadosNecesarios && objetosDesactivados == objetosDesactivadosNecesarios)
-        {
-            Debug.Log("Se han activado " + objetosActivadosNecesarios + " objetos y desactivado " + objetosDesactivadosNecesarios + " objetos.");
+            Debug.Log(objetoControlado.name + " está apagado.");
         }
     }
 }
